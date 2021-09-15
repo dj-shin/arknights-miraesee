15635c20887cfa111fef2d2829e60e38219021db
DongJin Shin <dongjin.shin.00@gmail.com>
2021-09-15 10:18:37 +0900

    feat: Header

---
+++ src/components/BannerCalendar.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const BannerCalendar: React.FunctionComponent = () => {
+    return (<div>calendar</div>);
+}
+export default BannerCalendar;
---
+++ src/components/Header.tsx
@@ -0,0 +1,37 @@
+import React, {useState} from "react";
+import permit from '../resources/Headhunting_Permit.png';
+import originium from '../resources/Pure_Originium.png';
+import orundum from '../resources/Orundum.png';
+import './Header.scss';
+
+interface ResourceEditorProps {
+    icon: any;
+    name: string;
+    amount: number;
+}
+const ResourceEditor: React.FunctionComponent<ResourceEditorProps> = (props) => {
+    const [amount, setAmount] = useState(props.amount);
+    return (
+        <div className="resource-editor">
+            <div className="container">
+                <img src={props.icon} alt={props.name}/>
+                <span className="amount">{amount}</span>
+                <button onClick={() => setAmount(amount + 1)}>+</button>
+            </div>
+        </div>
+    );
+}
+
+const Header: React.FunctionComponent = () => {
+    return (
+        <div id="header">
+            <div id="title">미래시</div>
+            <div id="resource">
+                <ResourceEditor name="cards" amount={9} icon={permit}/>
+                <ResourceEditor name="originium" amount={10} icon={originium}/>
+                <ResourceEditor name="orundum" amount={2800} icon={orundum}/>
+            </div>
+        </div>
+    );
+}
+export default Header;
